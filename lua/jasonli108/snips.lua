f26a5c28e0267a029bea7901b3eca595d4698e5e
Jason Li <jasonli108@gmail.com>
2024-09-02 16:49:24 -0400

    big file

--- lua/jasonli108/snips.lua
+++ lua/jasonli108/snips.lua
@@ -3,8 +3,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 
-ls.add_snippets('python', { s('main', t('if __name__ == "__main__"'))})
+ls.add_snippets('python', { s('main', t('if __name__ == "__main__":'))})
 ls.add_snippets('python', { s('init', t('def __init__(self): ' ))})
 ls.add_snippets('python', { s('ss', t('ss= s=Solution() ' ))})
 ls.add_snippets('python', { s('sprint', t(' print("some:{}, some2:{}".format(arr,r))'))})
-
