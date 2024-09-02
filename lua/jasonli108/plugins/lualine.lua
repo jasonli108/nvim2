2e15901c929d3c5b5f31f4fcb48df34a8e252858
Jason Li <jasonli108@gmail.com>
2024-09-02 13:13:26 -0400

    new lualine

--- lua/jasonli108/plugins/lualine.lua
+++ lua/jasonli108/plugins/lualine.lua
@@ -1,69 +1,36 @@
+-- Status line
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-    local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
+  -- https://github.com/nvim-lualine/lualine.nvim
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    -- https://github.com/nvim-tree/nvim-web-devicons
+    'nvim-tree/nvim-web-devicons', -- fancy icons
+    -- https://github.com/linrongbin16/lsp-progress.nvim
+    'linrongbin16/lsp-progress.nvim', -- LSP loading progress
+  },
+  opts = {
+    options = {
+      -- For more themes, see https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+      theme = "codedark", -- "auto, tokyonight, catppuccin, codedark, nord" 
+    },
+    sections = {
+      lualine_c = {
+        {
+          -- Customize the filename part of lualine to be parent/filename
+          'filename',
+          file_status = true,      -- Displays file status (readonly status, modified status)
+          newfile_status = false,  -- Display new file status (new file means no write after created)
+          path = 4,                -- 0: Just the filename
+                                   -- 1: Relative path
+                                   -- 2: Absolute path
+                                   -- 3: Absolute path, with tilde as the home directory
+                                   -- 4: Filename and parent dir, with tilde as the home directory
+          symbols = {
+            modified = '[+]',      -- Text to show when the file is modified.
+            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+          }
+        }
+      }
     }
-
-    local my_lualine_theme = {
-      normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-      },
-    }
-
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = my_lualine_theme,
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "filename", path = 1 },
-        },
-      },
-    })
-  end,
+  }
 }
--- lua/jasonli108/plugins/telescope.lua
+++ lua/jasonli108/plugins/telescope.lua
@@ -1,44 +1,46 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    { "nvim-telescope/telescope-ui-select.nvim" },
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+return {}
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown(),
-        },
-      },
-    })
-
-    telescope.load_extension("fzf")
-    telescope.load_extension("ui-select")
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-    local builtin = require("telescope.builtin")
-    keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-  end,
-}
+-- return {
+--   "nvim-telescope/telescope.nvim",
+--   branch = "0.1.x",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+--     "nvim-tree/nvim-web-devicons",
+--     { "nvim-telescope/telescope-ui-select.nvim" },
+--   },
+--   config = function()
+--     local telescope = require("telescope")
+--     local actions = require("telescope.actions")
+--
+--     telescope.setup({
+--       defaults = {
+--         path_display = { "truncate " },
+--         mappings = {
+--           i = {
+--             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+--             ["<C-j>"] = actions.move_selection_next, -- move to next result
+--             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+--           },
+--         },
+--       },
+--       extensions = {
+--         ["ui-select"] = {
+--           require("telescope.themes").get_dropdown(),
+--         },
+--       },
+--     })
+--
+--     telescope.load_extension("fzf")
+--     telescope.load_extension("ui-select")
+--
+--     -- set keymaps
+--     local keymap = vim.keymap -- for conciseness
+--     local builtin = require("telescope.builtin")
+--     keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+--     keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+--     keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Find string in cwd" })
+--     keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+--     keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+--   end,
+-- }
