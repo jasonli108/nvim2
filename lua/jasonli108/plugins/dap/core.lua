feb66b47b6b835d0d110f75b0908bd485dc0cecc
Jason Li <jasonli108@gmail.com>
2024-09-02 15:15:28 -0400

    python dap working

--- lua/jasonli108/plugins/dap/core.lua
+++ lua/jasonli108/plugins/dap/core.lua
@@ -1,117 +1,12 @@
+-- Debugging Support
 return {
-  {
-    "mfussenegger/nvim-dap",
+  -- https://github.com/rcarriga/nvim-dap-ui
+  'rcarriga/nvim-dap-ui',
+  event = 'VeryLazy',
+  dependencies = {
+    -- https://github.com/mfussenegger/nvim-dap
+    {'mfussenegger/nvim-dap',
 
-    dependencies = {
-
-      -- fancy UI for the debugger
-      {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "nvim-neotest/nvim-nio" },
-      -- stylua: ignore
-      keys = {
-        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-      },
-        opts = {},
-        config = function(_, opts)
-          -- setup dap config by VsCode launch.json file
-          -- require("dap.ext.vscode").load_launchjs()
-          local dap = require("dap")
-          local dapui = require("dapui")
-          dapui.setup(opts)
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open({})
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close({})
-          end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close({})
-          end
-        end,
-      },
-
-      -- virtual text for the debugger
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
-      },
-
-      -- which key integration
-      {
-        "folke/which-key.nvim",
-        optional = true,
-        opts = {
-          defaults = {
-            ["<leader>d"] = { name = "+debug" },
-          },
-        },
-      },
-
-      -- mason.nvim integration
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = "mason.nvim",
-        cmd = { "DapInstall", "DapUninstall" },
-        opts = {
-          -- Makes a best effort to setup the various debuggers with
-          -- reasonable debug configurations
-          automatic_installation = true,
-
-          -- You can provide additional configuration to the handlers,
-          -- see mason-nvim-dap README for more information
-          handlers = {},
-
-          -- You'll need to check that you have the required things installed
-          -- online, please don't ask me how to install them :)
-          ensure_installed = {
-            -- Update this to ensure that you have the debuggers for the langs you want
-          },
-        },
-      },
-
-      {
-        "jbyuki/one-small-step-for-vimkind",
-  -- stylua: ignore
-  config = function()
-    local dap = require("dap")
-    dap.adapters.nlua = function(callback, conf)
-      local adapter = {
-        type = "server",
-        host = conf.host or "127.0.0.1",
-        port = conf.port or 8086,
-      }
-      if conf.start_neovim then
-        local dap_run = dap.run
-        dap.run = function(c)
-          adapter.port = c.port
-          adapter.host = c.host
-        end
-        require("osv").run_this()
-        dap.run = dap_run
-      end
-      callback(adapter)
-    end
-    dap.configurations.lua = {
-      {
-        type = "nlua",
-        request = "attach",
-        name = "Run this file",
-        start_neovim = {},
-      },
-      {
-        type = "nlua",
-        request = "attach",
-        name = "Attach to running Neovim instance (port = 8086)",
-        port = 8086,
-      },
-    }
-  end,
-      },
-    },
-
-  -- stylua: ignore
   keys = {
     { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
     { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
@@ -151,5 +46,126 @@ return {
         )
       end
     end,
+
+    },
+    -- https://github.com/mfussenegger/nvim-dap-python
+    {'mfussenegger/nvim-dap-python',
+      ft='python',
+      dependencies ={
+        'mfussenegger/nvim-dap',
+        'rcarriga/nvim-dap-ui',
+      },
+      config = function(_, opts)
+        local path ="~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+        require('dap-python').setup(path)
+      end,
+    },
+    -- https://github.com/nvim-neotest/nvim-nio
+    'nvim-neotest/nvim-nio',
+    -- https://github.com/theHamsta/nvim-dap-virtual-text
+    'theHamsta/nvim-dap-virtual-text', -- inline variable text while debugging
+    -- https://github.com/nvim-telescope/telescope-dap.nvim
+    'nvim-telescope/telescope-dap.nvim', -- telescope integration with dap
   },
+  opts = {
+    controls = {
+      element = "repl",
+      enabled = false,
+      icons = {
+        disconnect = "",
+        pause = "",
+        play = "",
+        run_last = "",
+        step_back = "",
+        step_into = "",
+        step_out = "",
+        step_over = "",
+        terminate = ""
+      }
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {
+      border = "single",
+      mappings = {
+        close = { "q", "<Esc>" }
+      }
+    },
+    force_buffers = true,
+    icons = {
+      collapsed = "",
+      current_frame = "",
+      expanded = ""
+    },
+    layouts = {
+      {
+        elements = {
+          {
+            id = "scopes",
+            size = 0.50
+          },
+          {
+            id = "stacks",
+            size = 0.30
+          },
+          {
+            id = "watches",
+            size = 0.10
+          },
+          {
+            id = "breakpoints",
+            size = 0.10
+          }
+        },
+        size = 40,
+        position = "left", -- Can be "left" or "right"
+      },
+      {
+        elements = {
+          "repl",
+          "console",
+        },
+        size = 10,
+        position = "bottom", -- Can be "bottom" or "top"
+      }
+    },
+    mappings = {
+      edit = "e",
+      expand = { "<CR>", "<2-LeftMouse>" },
+      open = "o",
+      remove = "d",
+      repl = "r",
+      toggle = "t"
+    },
+    render = {
+      indent = 1,
+      max_value_lines = 100
+    }
+  },
+  config = function (_, opts)
+    local dap = require('dap')
+    require('dapui').setup(opts)
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      require('dapui').open()
+    end
+
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      -- Commented to prevent DAP UI from closing when unit tests finish
+      require('dapui').close()
+    end
+
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      -- Commented to prevent DAP UI from closing when unit tests finish
+      require('dapui').close()
+    end
+
+    -- Add dap configurations based on your language/adapter settings
+    -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+    -- dap.configurations.xxxxxxxxxx = {
+    --   {
+    --   },
+    -- }
+  end
 }
+
