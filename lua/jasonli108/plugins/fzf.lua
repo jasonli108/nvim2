cd2cf92a99898ea75ff4f97ce510d53e5c496226
Jason Li <jasonli108@gmail.com>
2024-09-08 13:44:51 -0400

    updated fzf

--- lua/jasonli108/plugins/fzf.lua
+++ lua/jasonli108/plugins/fzf.lua
@@ -1,4 +1,4 @@
-  kind_filter = {
+kind_filter = {
     default = {
       "Class",
       "Constructor",
@@ -48,7 +48,12 @@ end
 
 return {
   "ibhagwan/fzf-lua",
-  dependencies = {'neovim/nvim-lspconfig',},
+  dependencies = {
+      -- 'neovim/nvim-lspconfig',
+{"nvim-tree/nvim-web-devicons"},
+      -- { 'mfussenegger/nvim-dap',}
+    --
+    },
   cmd = "FzfLua",
   opts = function(_, opts)
     local actions = require('fzf-lua.actions')
@@ -114,53 +119,81 @@ return {
 end,
 
   keys = {
-    { "<c-j>", "<c-j>", ft = "fzf", mode = "t", nowait = true },
-    { "<c-k>", "<c-k>", ft = "fzf", mode = "t", nowait = true },
-    {
-      "<leader>,",
-      "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
-      desc = "Switch Buffer",
-    },
-    { "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
-    -- find
-    { "<leader>fw", "<cmd>FzfLua lsp_document_diagnostics<cr>", desc = "lsp diagnostics_document" },
-    { "<leader>fW", "<cmd>FzfLua lsp_workspace_diagnostics<cr>", desc = "lsp_workspace_diagnostics" },
-    { "<leader>fs", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-    { "<leader>fg", "<cmd>FzfLua live_grep<cr>" ,desc = "Grep (Root Dir)" },
-    { "<leader>fG", "<cmd>FzfLua live_grep_resume<cr>", desc = "Grep (Root Dir)" },
 
-    { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files (Root Dir)" },
-    { "<leader>fF", "<cmd>FzfLua files_resume<cr>", desc = "Find Files (Root Dir) Resume" },
-    { "<leader>fd", "<cmd>FzfLua buffers<cr>", desc = "Find Buffer (Root Dir)" },
-    { "<leader>fD", "<cmd>FzfLua buffers_resume<cr>", desc = "Find Buffer (Root Dir) Resume" },
-    { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
-    -- git
-    { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
-    { "<leader>gg", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
-    { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
-    -- search
-    { '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
-    { "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
-    { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
-    { "<leader>sc", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
-    { "<leader>sC", "<cmd>FzfLua commands<cr>", desc = "Commands" },
-    { "<leader>sd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
-    { "<leader>sD", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
-
-
-    { "<leader>sh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
-    { "<leader>sH", "<cmd>FzfLua highlights<cr>", desc = "Search Highlight Groups" },
-    { "<leader>sj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
-    { "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
-    { "<leader>sl", "<cmd>FzfLua loclist<cr>", desc = "Location List" },
-    { "<leader>sM", "<cmd>FzfLua man_pages<cr>", desc = "Man Pages" },
-    { "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
-    { "<leader>sR", "<cmd>FzfLua resume<cr>", desc = "Resume" },
-    { "<leader>sq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
+      { "<c-j>", "<c-j>", ft = "fzf", mode = "t", nowait = true },
+      { "<c-k>", "<c-k>", ft = "fzf", mode = "t", nowait = true },
+      {
+        "<leader>,",
+        "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
+        desc = "Switch Buffer",
+      },
+      { "<leader>/", "<cmd>FzfLua live_grep<cr>", desc = "Grep (Root Dir)" },
+      { "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+      { "<leader><space>", "<cmd>FzfLua files<cr>", desc = "Find Files (Root Dir)" },
+      -- find
+      { "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+      { "<leader>fc", "<cmd>FzfLua config_files()<cr>", desc = "Find Config File" },
+      { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files (Root Dir)" },
+      { "<leader>fF", "<cmd>FzfLua files({ root = false })<cr>", desc = "Find Files (cwd)" },
+      { "<leader>fg", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+      { "<leader>fR", "<cmd>FzfLua oldfiles({ cwd = vim.uv.cwd() })<cr>", desc = "Recent (cwd)" },
+      -- git
+      { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
+      { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
+      -- search
+      { '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
+      { "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
+      { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+      { "<leader>sC", "<cmd>FzfLua commands<cr>", desc = "Commands" },
+      { "<leader>sd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Grep (Root Dir)" },
+      { "<leader>sG", "<cmd>FzfLua live_grep({ root = false })<cr>",  desc = "Grep (cwd)" },
+      { "<leader>sh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
+      { "<leader>sH", "<cmd>FzfLua highlights<cr>", desc = "Search Highlight Groups" },
+      { "<leader>sj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
+      { "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
+      { "<leader>sl", "<cmd>FzfLua loclist<cr>", desc = "Location List" },
+      { "<leader>sM", "<cmd>FzfLua man_pages<cr>", desc = "Man Pages" },
+      { "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
+      { "<leader>sR", "<cmd>FzfLua resume<cr>", desc = "Resume" },
+      { "<leader>sq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
+      { "<leader>sw", "<cmd>FzfLua grep_cword<cr>", desc = "Word (Root Dir)" },
+      { "<leader>sW", "<cmd>FzfLua grep_cword({ root = false })<cr>", desc = "Word (cwd)" },
+      { "<leader>sw", "<cmd>FzfLua grep_visual<cr>", desc = "Selection (Root Dir)" },
+      { "<leader>sW", "<cmd>FzfLua grep_visual({ root = false })<cr>",  desc = "Selection (cwd)" },
+      { "<leader>uC", "<cmd>FzfLua colorschemes<cr>", desc = "Colorscheme with Preview" },
+      {
+        "<leader>ss",
+        function()
+          require("fzf-lua").lsp_document_symbols({
+            regex_filter = symbols_filter,
+          })
+        end,
+        desc = "Goto Symbol",
+      },
+      {
+        "<leader>sS",
+        function()
+          require("fzf-lua").lsp_live_workspace_symbols({
+            regex_filter = symbols_filter,
+          })
+        end,
+        desc = "Goto Symbol (Workspace)",
+      },
 
     --lsp
-    -- { "<leader>gd", "<cmd>FzfLua lsp_definitions<cr>", desc = "go to def" },
+    { "gd", "<cmd>FzfLua lsp_definitions     jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto Definition"},
+    { "gr", "<cmd>FzfLua lsp_references      jump_to_single_result=true ignore_current_line=true<cr>", desc = "References", nowait = true },
+    { "gI", "<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto Implementation" },
+    { "gy", "<cmd>FzfLua lsp_typedefs        jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto T[y]pe Definition" },
 
+        -- { "<leader>d", "<cmd>FzfLua lsp_document_diagnostics<CR>", desc = "lsp_document_diagnostics" },
+        { "<leader>gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "lsp_definition" },
+        { "<leader>gr", "<cmd>FzfLua lsp_references<CR>", desc = "lsp_references" },
+        { "<leader>gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "lsp_implementations" },
         {
       "<leader>ss",
       function()
@@ -181,21 +214,6 @@ end,
     },
   },
 
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = function()
-  --     local lsp_attach = function(client, bufnr)
-  --       local keymap = vim.keymap -- for conciseness
-  --       -- stylua: ignore
-  --       vim.list_extend(keymap, {
-  --         { "gd", "<cmd>FzfLua lsp_definitions     jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto Definition", has = "definition" },
-  --         { "gr", "<cmd>FzfLua lsp_references      jump_to_single_result=true ignore_current_line=true<cr>", desc = "References", nowait = true },
-  --         { "gI", "<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto Implementation" },
-  --         { "gy", "<cmd>FzfLua lsp_typedefs        jump_to_single_result=true ignore_current_line=true<cr>", desc = "Goto T[y]pe Definition" },
-  --       })
-  --     end
-  --   end
-  -- },
 }
 
 
