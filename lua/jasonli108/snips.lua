608612e9384e77f8e9fcfeb1e78661cbe29b0595
Jason Li <jasonli108@gmail.com>
2024-09-15 23:26:19 -0400

    update snip to include main with solution

--- lua/jasonli108/snips.lua
+++ lua/jasonli108/snips.lua
@@ -2,8 +2,27 @@ local ls = require("luasnip")
 -- some shorthands...
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
-ls.add_snippets('python', { s('main', t('if __name__ == "__main__":'))})
-ls.add_snippets('python', { s('init', t('def __init__(self): ' ))})
-ls.add_snippets('python', { s('ss', t('ss= s=Solution() ' ))})
-ls.add_snippets('python', { s('sprint', t(' print("some:{}, some2:{}".format(arr,r))'))})
+ls.add_snippets("python", {
+  s({ trig = "main", name = "main header", desc = "check if __name__ is equal to the __main__" }, {
+    t('if __name__ == "__main__":'),
+  }),
+})
+
+ls.add_snippets("python", {
+  s({ trig = "constructor", name = "constructor", desc = "constructor header" }, {
+    t({ "", "  " }),
+    i(0, "def __init__(self): "),
+  }),
+})
+
+ls.add_snippets("python", {
+  s({ trig = "mainss", name = "main with ss", desc = "main with solution" }, {
+    t('if __name__ == "__main__":'),
+    t({ "", "  " }),
+    i(0, "ss = Solution() "),
+    t({ "", "  " }),
+    i(0, "assert ss.function==expect_result"),
+  }),
+})
